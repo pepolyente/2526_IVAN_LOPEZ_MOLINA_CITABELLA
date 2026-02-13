b22088ed4f21b3abc236493d3870c5035b9bea4d
shdwd <lopez.accs@gmail.com>
2026-02-14 00:54:52 +0100

    feat: AppointmentResponse updated

--- backend/CitaBellaAPI/src/main/java/com/citabella/citabellaapi/dto/appointment/AppointmentResponse.java
+++ backend/CitaBellaAPI/src/main/java/com/citabella/citabellaapi/dto/appointment/AppointmentResponse.java
@@ -1,7 +1,22 @@
 package com.citabella.citabellaapi.dto.appointment;
 
+import com.citabella.citabellaapi.dto.client.ClientResponse;
+import com.citabella.citabellaapi.dto.employee.EmployeeResponse;
+import com.citabella.citabellaapi.dto.treatment.TreatmentResponse;
+import com.citabella.citabellaapi.entity.enums.AppointmentStatus;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
 public record AppointmentResponse(
         Integer id,
-        boolean hasOverlap
+        LocalDateTime startAt,
+        LocalDateTime endAt,
+        AppointmentStatus status,
+        String notes,
+        boolean hasOverlap,
+        ClientResponse client,
+        EmployeeResponse employee,
+        Set<TreatmentResponse> treatments
 ) {
 }
